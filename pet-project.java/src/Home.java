919ec965aff3d788babf9f376324bea089714cbe
EminLyv <qabalaqamarvan2005@gmail.com>
2023-12-19 19:45:37 +0000

    added pet-projects.java

--- pet-project.java/src/Home.java
+++ pet-project.java/src/Home.java
@@ -3,12 +3,14 @@ public class Home {
     int sahe;
     boolean bina;
     String location;
+    int cost;
 
     //Constructor :
-    public Home(int sahe, boolean bina, String location){
+    public Home(int sahe, boolean bina, String location, int cost){
         this.sahe = sahe;
         this.bina = bina;
         this.location = location;
+        this.cost = cost;
     }
 
     public Home() {
--- pet-project.java/src/Main.java
+++ pet-project.java/src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Home FirstHome = new Home(43,false,"Yasamal");
-        System.out.println(FirstHome.sahe);
+        Home FirstHome = new Home(43,false,"Yasamal",467);
+        FirstHome.cost = 450;
+        System.out.println(FirstHome.cost);
 
         Home SecondHome = new Home();
         SecondHome.sahe = 37;
         SecondHome.bina = true;
         SecondHome.location = "Nesimi";
-        System.out.println(SecondHome.sahe);
+        SecondHome.cost = 430;
+        System.out.println(SecondHome.cost);
     }
 }
